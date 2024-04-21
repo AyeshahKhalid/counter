2a93bc4402807c354624cb60fd3aa2f9b1120099
Ayesha Khalid <ayeshakhalid289@gmail.com>
2024-04-21 16:33:31 +0500

    use dispatch and selector to inc, dec

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { counter_increment, counter_decrement } from '../redux/action'
 
 export default function Counter(props) {
-
+    const dispatch = useDispatch()
+    const counter = useSelector(state => state.counter)
     return (
         <div className='container'>
 
             <h1>Counter Application</h1>
-            <h2>0</h2>
-            <div className='counter'> 
-                <button className='increment'>Increment</button>
-                <button className='decrement'>Decrement</button>
+            <h2>{counter}</h2>
+            <div className='counter'>
+                <button className='increment' onClick={() => dispatch(counter_increment())}>Increment</button>
+                <button className='decrement' onClick={() => dispatch(counter_decrement())}>Decrement</button>
             </div>
         </div>
     )
